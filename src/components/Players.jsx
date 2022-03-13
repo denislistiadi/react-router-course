d5d0a23204ca66bd741e9c2ca23d5bed36636e6e
denis listiadi <denislistiadi24@gmail.com>
2022-03-13 20:14:35 +0700

    Players

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,15 +1,76 @@
 import React from 'react'
-import {useLocation, useSearchParams} from 'react-router-dom'
+import {useLocation, useSearchParams, Link} from 'react-router-dom'
+import usePlayerNames from '../hooks/usePlayerNames'
+import { slugify } from '../utils'
+
+function CustomLink ({ to, children }) {
+  const location = useLocation()
+  const playerId = location.pathname.split('/')[2]
+  const match = playerId === to
+
+  const styles = match === true ? {
+    fontWeight: 900, color: 'var(--white)'
+  } : {}
+
+  return (
+    <li>
+      <Link 
+        styles={{...styles}} 
+        to={{
+          pathname: to,
+          search: location.search
+        }}
+      >
+        {children}
+      </Link>
+    </li>
+  )
+}
+
+function Sidebar ({ title, list }) {
+  return (
+    <div>
+      <h3 className='header'>{title}</h3>
+      <ul className='sidebar-list'>
+        {list.map(item => (
+          <CustomLink key={item} to={slugify(item)}>
+            {item.toUpperCase()}
+          </CustomLink>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
 function Players() {
   const location = useLocation()
   const [searchParams] = useSearchParams(location.search)
-  
-  const team = searchParams.get('teamId')
+  const [team, setTeam] = React.useState(
+    searchParams.get('teamId')
+  )
+
+  React.useEffect (() => {
+    if (location.search === '') {
+      searchParams.delete('teamId')
+      setTeam(null)
+    } else {
+      setTeam(searchParams.get('teamId'))
+    }
+  }, [location.search, searchParams])
+
+
+  const {response: names, loading} = usePlayerNames(team)
+
+  if (loading === true) {
+    return null
+  }
 
   return (
     <div className='container'>
-        Players for team {team}
+        <Sidebar
+          title='Players'
+          list={names}
+        />
     </div>
   )
 }
