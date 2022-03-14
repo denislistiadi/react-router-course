be783d7dedbfd220f5f4d2002c4a708e260ad046
denis listiadi <denislistiadi24@gmail.com>
2022-03-14 17:38:27 +0700

    Programmatically Navigate

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,43 +1,58 @@
-import { useParams, Link } from 'react-router-dom';
-import usePlayer from '../hooks/usePlayer';
-import Loading from './Loading';
+import { useParams, Link, Navigate } from "react-router-dom";
+import usePlayer from "../hooks/usePlayer";
+import Loading from "./Loading";
 
 export default function Player() {
-    const { playerId } = useParams();
-    const { response: player, loading } = usePlayer(playerId);
+  const { playerId } = useParams();
+  const { response: player, loading } = usePlayer(playerId);
 
-    if (loading === true) {
-        return <Loading />
-    }
-
-    return (
-        <div className="panel">
-            <img 
-                className='avatar'
-                src={player.avatar}   
-                alt={`Avatar for ${player.name}`} 
-            />
-            <h1 className="medium-header">{player.name}</h1>
-            <h3 className="header">#{player.number}</h3>
-            <div className="row">
-                <ul className="info-list" style={{marginRight: 80}}>
-                    <li>
-                        Team
-                        <div>
-                            <Link to={`/${player.teamId}`}>
-                                {player.teamId[0].toUpperCase() + player.teamId.slice(1)}
-                            </Link>
-                        </div>
-                    </li>
-                    <li>Position<div>{player.position}</div></li>
-                    <li>PPG<div>{player.ppg}</div></li>
-                </ul>
-                <ul className="info-list">
-                    <li>APG<div>{player.apg}</div></li>
-                    <li>SRG<div>{player.spg}</div></li>
-                    <li>RPG<div>{player.rpg}</div></li>
-                </ul>
-            </div>
+  let body;
+  if (loading === true) {
+    body = <Loading />;
+  } else if (player === null) {
+    body = <Navigate to={`/players`} />;
+  } else {
+    body = (
+      <>
+        <img
+          className="avatar"
+          src={player.avatar}
+          alt={`Avatar for ${player.name}`}
+        />
+        <h1 className="medium-header">{player.name}</h1>
+        <h3 className="header">#{player.number}</h3>
+        <div className="row">
+          <ul className="info-list" style={{ marginRight: 80 }}>
+            <li>
+              Team
+              <div>
+                <Link to={`/${player.teamId}`}>
+                  {player.teamId[0].toUpperCase() + player.teamId.slice(1)}
+                </Link>
+              </div>
+            </li>
+            <li>
+              Position<div>{player.position}</div>
+            </li>
+            <li>
+              PPG<div>{player.ppg}</div>
+            </li>
+          </ul>
+          <ul className="info-list">
+            <li>
+              APG<div>{player.apg}</div>
+            </li>
+            <li>
+              SRG<div>{player.spg}</div>
+            </li>
+            <li>
+              RPG<div>{player.rpg}</div>
+            </li>
+          </ul>
         </div>
-    )
+      </>
+    );
+  }
+
+  return <div className="panel">{body}</div>;
 }
--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, Navigate } from 'react-router-dom';
 import useTeam from '../hooks/useTeam';
 import Loading from './Loading';
 import TeamLogo from './TeamLogo';
@@ -8,28 +8,34 @@ export default function Team() {
 
     const {loading, response: team} = useTeam(teamId);
 
+    let body;
+    if (loading === true) {
+        body = <Loading />;
+    } else if (team === null) {
+        body = <Navigate to={`/teams`} />;
+    } else {
+        body = (
+            <div style={{width: '100%'}}>
+                <TeamLogo id={team.id} className='center' />
+                <h1 className="medium-header">{team.name}</h1>
+                <ul className="info-list row">
+                    <li>Est.<div>{}team.established</div></li>
+                    <li>Manager<div>{}team.manager</div></li>
+                    <li>Coach<div>{}team.coach</div></li>
+                </ul>
+                <Link
+                    to={`/${teamId}`}
+                    className="center btn-main"
+                >
+                    {team.name} Team Page
+                </Link>
+            </div>
+        )
+    }
+
     return (
         <div className="panel">
-            {loading === true 
-            ?   <Loading />
-            :   (
-                <div style={{width: '100%'}}>
-                    <TeamLogo id={team.id} className='center' />
-                    <h1 className="medium-header">{team.name}</h1>
-                    <ul className="info-list row">
-                        <li>Est.<div>{}team.established</div></li>
-                        <li>Manager<div>{}team.manager</div></li>
-                        <li>Coach<div>{}team.coach</div></li>
-                    </ul>
-                    <Link
-                        to={`/${teamId}`}
-                        className="center btn-main"
-                    >
-                        {team.name} Team Page
-                    </Link>
-                </div>
-                )
-            }
+            {body}
         </div>
     )
 }
